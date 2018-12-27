a8d2eae31f7002f72853cbf589d07416d8c60ea3
bianhaonan <hnbian@126.com>
2018-12-27 15:57:31 +0800

    创建Spark工具类，增加获取SparkSession 方法

---
+++ src/main/scala/hnbian/spark/SparkUtils.scala
@@ -0,0 +1,22 @@
+package hnbian.spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+
+
+/**
+  * @author hnbian
+  * @ Description 获取SparkSession
+  * @ Date 2018/12/27 15:43
+  **/
+object SparkUtils {
+
+  def getSparkSession(appName:String,workers:Int): SparkSession ={
+    val conf = new SparkConf().setAppName(appName)
+    //设置master local[worker] 指定本地模式开启模拟worker线程数
+    conf.setMaster(s"local[${workers}]")
+    //创建sparkContext文件
+    val sc = new SparkContext(conf)
+    SparkSession.builder().getOrCreate()
+  }
+}
