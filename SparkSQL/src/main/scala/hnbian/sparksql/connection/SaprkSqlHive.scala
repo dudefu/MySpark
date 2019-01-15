6856c747afb739009d50a866e42fa923cf96c4cd
bianhaonan <hnbian@126.com>
2019-01-15 17:50:45 +0800

    saprk sql 连接hive

---
+++ SparkSQL/src/main/scala/hnbian/sparksql/connection/SaprkSqlHive.scala
@@ -0,0 +1,22 @@
+package hnbian.sparksql.connection
+
+import java.sql.DriverManager
+
+/**
+  * @author hnbian
+  *         @ Description 
+  *         @ Date 2019/1/15 17:43
+  **/
+object SaprkSqlHive extends App {
+  //注册驱动
+  Class.forName("org.apache.hive.jdbc.HiveDriver")
+  //val conn = DriverManager.getConnection("jdbc:hive2://master1:10000")
+  val conn = DriverManager.getConnection("jdbc:hive2://master:10000")
+  val st = conn.createStatement()
+  //val rs = st.executeQuery("show databases")
+  val rs = st.executeQuery("select count(1) from tableName")
+  while(rs.next()){
+    println(rs.getString(1))
+  }
+  println("over! . . .")
+}
